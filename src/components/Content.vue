32552df9a5cd06d2223eaaf9127ce4784714cd73
Lexi Sother <lexisoth2005@gmail.com>
2021-04-17 18:32:38 +0000

    Added small projects list

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -10,15 +10,46 @@
         >
       </li>
       <li>
+        <i class="fab fa-twitter fa-lg"></i>&nbsp;
+        <a href="https://twitter.com/lexisother" target="_blank" rel="noopener"
+          >Twitter</a
+        >
+      </li>
+    </ul>
+
+    <h3>Projects</h3>
+    <ul>
+      <li>
         <i class="fab fa-github fa-lg"></i>&nbsp;
         <a href="https://github.com/NovaGM" target="_blank" rel="noopener"
           >NovaGM</a
         >
       </li>
       <li>
-        <i class="fab fa-twitter fa-lg"></i>&nbsp;
-        <a href="https://twitter.com/lexisother" target="_blank" rel="noopener"
-          >Twitter</a
+        <i class="fas fa-code-branch fs-lg"></i>&nbsp;
+        <a
+          href="https://github.com/lexisother/lexisother.tk"
+          target="_blank"
+          rel="noopener"
+          >Website</a
+        >
+      </li>
+      <li>
+        <i class="fas fa-dog fs-lg"></i>&nbsp;
+        <a
+          href="https://github.com/keanuplayz/TravBot-v3"
+          target="_blank"
+          rel="noopener"
+          >TravBot-v3</a
+        >
+      </li>
+      <li>
+        <i class="fas fa-clipboard-list fa-lg"></i>&nbsp;
+        <a
+          href="https://github.com/keanuplayz/LoggerTS/tree/webhooks"
+          target="_blank"
+          rel="noopener"
+          >LoggerTS</a
         >
       </li>
     </ul>
