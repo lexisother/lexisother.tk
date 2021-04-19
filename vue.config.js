25a3dc27f6d1366720a77fc640dae8cacc02d885
Lexi Sother <lexisoth2005@gmail.com>
2021-04-19 17:19:26 +0000

    Removed patch-package and comments in Vue config

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,3 @@
-// // eslint-disable-next-line @typescript-eslint/no-var-requires
-// let fs = require("fs");
-
-// const AboutContent = fs.readFileSync("./src/assets/test.md");
-
-// module.exports = {
-//   chainWebpack: (config) => {
-//     config.plugin("define").tap((args) => {
-//       let _base = args[0]["process.env"];
-//       args[0]["process.env"] = {
-//         ..._base,
-//         ABOUT_CONTENT: AboutContent,
-//       };
-//       return args;
-//     });
-//   },
-// };
-
 module.exports = {
   chainWebpack: (config) => {
     config.module
