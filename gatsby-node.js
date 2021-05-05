9cb4e39299035a515a7e4ae122f73b682de68d75
Alyxia Sother <lexisoth2005@gmail.com>
2021-05-05 13:28:56 +0000

    [Blog] Added autogenerated blog post page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,18 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 // @ts-check
-
 const path = require("path");
 
 /**
-
  * @type {import('gatsby').GatsbyNode}
-
  */
-
 module.exports = {
     onCreateNode: ({actions, node}) => {
         if (node.internal.type === "MarkdownRemark") {
             // Add slug to markdown files
-
             // @ts-expect-error: node.fileAbsolutePath is unknown
-
             const value = path.basename(path.dirname(node.fileAbsolutePath));
-
             actions.createNodeField({
                 node,
-
                 name: "slug",
-
                 value
             });
         }
@@ -44,9 +33,9 @@ module.exports = {
         });
 
         actions.createPage({
-          path: '/blog',
-          component: path.resolve('./src/Blog.tsx'),
-          context: null
+            path: "/blog",
+            component: path.resolve("./src/Blog.tsx"),
+            context: null
         });
 
         actions.createPage({
@@ -56,40 +45,38 @@ module.exports = {
         });
 
         // Generate pages for blog posts
-        // const queryResult = await graphql(`
-        //   query {
-        //     allMarkdownRemark {
-        //       nodes {
-        //         fields {
-        //           slug
-        //         }
-        //       }
-        //     }
-        //   }
-        // `);
-        //
+        const queryResult = await graphql(`
+            query {
+                allMarkdownRemark {
+                    nodes {
+                        fields {
+                            slug
+                        }
+                    }
+                }
+            }
+        `);
 
-        // if (queryResult.errors) throw queryResult.errors;
+        if (queryResult.errors) throw queryResult.errors;
 
-        // /**
-        //  * @type {GatsbyTypes.MarkdownRemarkConnection}
-        //  */
+        /**
+         * @type {GatsbyTypes.MarkdownRemarkConnection}
+         */
+        const blogPostConnection = queryResult.data.allMarkdownRemark;
 
-        // const blogPostConnection = queryResult.data.allMarkdownRemark;
+        blogPostConnection.nodes.forEach((node) => {
+            if (!node.fields) {
+                return;
+            }
 
-        // blogPostConnection.nodes.forEach((node) => {
-        //   if (!node.fields) {
-        //     return;
-        //   }
+            const {slug} = node.fields;
+            const coverImagePath = `blog/${slug}/Cover.png`;
 
-        //   const slug = node.fields.slug;
-        //   const coverImagePath = `blog/${slug}/Cover.png`;
-
-        //   actions.createPage({
-        //     path: '/blog/' + slug,
-        //     component: path.resolve('./src/BlogPost.tsx'),
-        //     context: { slug, coverImagePath }
-        //   });
-        // });
+            actions.createPage({
+                path: `/blog/${  slug}`,
+                component: path.resolve("./src/BlogPost.tsx"),
+                context: {slug, coverImagePath}
+            });
+        });
     }
 };
