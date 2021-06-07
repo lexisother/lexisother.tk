37d68b853ea0073029c9109c72b1ba3b0790b2fc
Alyxia Sother <lexisoth2005@gmail.com>
2021-06-07 19:54:00 +0000

    [Skills] Add tabs structure for About page

--- src/Skills.tsx
+++ src/Skills.tsx
@@ -1,23 +1,47 @@
 import {formatDistanceToNow as distance} from "date-fns";
 import React from "react";
+import {Tab, TabList, TabPanel, Tabs} from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import {genid} from "./infra/utils";
 import Page from "./shared/Page";
 
 export default function SkillsPage() {
     return (
         <Page title="Skills">
-            <div className="section-header">Skills.</div>
+            <div className="section-header">About me</div>
 
-            <div className="section-prelude">Here's an overview of my skills.</div>
-            <div>
-                <List list={skills} />
+            <div className="section-prelude">
+                This page contains various info about me, from skills to interests, it's all here!
             </div>
+            <Tabs>
+                <TabList>
+                    <Tab>Info</Tab>
+                    <Tab>Skills</Tab>
+                    <Tab>Interests</Tab>
+                </TabList>
+                <TabPanel>
+                    {/* TODO: Move parts of Home.tsx here and expand upon it */}
+                    <h2>[write stuff about myself here idk]</h2>
+                </TabPanel>
+                <TabPanel>
+                    <div>
+                        <List list={skills} />
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    {/* TODO: Do the same as `skills` for this one */}
+                    <h2>[list about interests]</h2>
+                </TabPanel>
+            </Tabs>
         </Page>
     );
 }
 
+// TODO: Literally everything below this point has to be moved to another file, it's atrocious to keep it here
+
 const List = ({list}: {list: Object[]}) => (
     <ul>
+        {/* If there's no list, provide an empty array */}
         {(list || []).map((item: any) => (
             <ListItem key={genid()} item={item} />
         ))}
@@ -54,6 +78,7 @@ interface Item {
     time: string;
 }
 
+// SkillArray of skills displayed with the `List` component
 const skills: Array<Skill> = [
     {
         name: "Programming",
